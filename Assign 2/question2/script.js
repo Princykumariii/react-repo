2fbf8e39d172da434696e3c20383cd40032c7d47
Your GitHub Username <Your Email>
2025-01-13 23:28:45 +0530

    quest2

--- Assign 2/question2/script.js
+++
@@ -1,3 +0,0 @@
-let element = React.createElement("div",{id:"container"},[
-    React.createElement("h1",{id:"heading"},"Welcome")
-])
--- Assign 2/question2/script2.js
+++
@@ -1,8 +0,0 @@
-let element = React.createElement("div",{id:"container"},[
-    React.createElement(
-      "h1" , {id:"heading",className:"mainHeading"},"Welcome Back"
-    ),
-    // React.createElement(
-        // "p",{id:"subheading",
-//     )
- ])
