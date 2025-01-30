cf7b478ed748a5d8a33b653e02f75aa3fa68f3a4
Your GitHub Username <Your Email>
2025-01-30 23:25:14 +0530

    qs2

---
+++ Routing /Routing/src/Comp/question1/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+import { Link } from "react-router-dom";
+//import "../Styles/Navbar.css"
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+<Link to="/" className="nav-link"> Home</Link>
+<Link to="/About" className="nav-link"> About</Link>
+
+<Link to="/Contact" className="nav-link"> Contact</Link>
+    </nav>
+  )
+}
+export default Navbar;
